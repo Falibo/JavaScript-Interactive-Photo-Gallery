3c13e7da5e2f3255bc3aaa8a3ed092c79325ab16
Falibo <100304112+Falibo@users.noreply.github.com>
2025-02-15 20:30:19 -0500

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,19 @@
+function upDate(previewPic) {
+    
+    console.log("Mouse over event triggered");
+    console.log("Image Source:", previewPic.src);
+    console.log("Image Alt Text:", previewPic.alt);
 
+    let imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = `url(${previewPic.src})`;
+    imageDiv.innerText = previewPic.alt;
+}
+
+function unDo() {
+    
+    console.log("Mouse out event triggered");
+
+    let imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = "url('')";
+    imageDiv.innerText = "Hover over an image below to display here.";
+}
